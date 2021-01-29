e32c61801a3e7f8639272a958e6c9a8cd8f84bf4
Aleksander Bies <aleksanderbies@gmail.com>
2021-01-29 16:24:17 +0100

    zad3

---
+++ kolokwium/kol_1/zad3/CMakeFiles/zadanie3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie3.dir/src/libtest.cpp.o"
+  "zadanie3"
+  "zadanie3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zadanie3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
