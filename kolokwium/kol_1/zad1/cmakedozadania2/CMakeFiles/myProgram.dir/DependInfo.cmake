e4ef2afc354ccc6bb0060d9ffc583b074ca25854
Aleksander Bies <aleksanderbies@gmail.com>
2021-01-29 16:17:04 +0100

    cmake zadanie2

---
+++ kolokwium/kol_1/zad1/cmakedozadania2/CMakeFiles/myProgram.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/address.cpp" "/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/cmakedozadania2/CMakeFiles/myProgram.dir/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/address.cpp.o"
+  "/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/employee.cpp" "/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/cmakedozadania2/CMakeFiles/myProgram.dir/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/employee.cpp.o"
+  "/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/main.cpp" "/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/cmakedozadania2/CMakeFiles/myProgram.dir/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/cmakedozadania2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ kolokwium/kol_1/zad1/cmakedozadania2/CMakeFiles/myProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProgram.dir/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/address.cpp.o"
+  "CMakeFiles/myProgram.dir/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/employee.cpp.o"
+  "CMakeFiles/myProgram.dir/home/aleksander/bies_kolokwium/kolokwium/kol_1/zad1/main.cpp.o"
+  "myProgram"
+  "myProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
